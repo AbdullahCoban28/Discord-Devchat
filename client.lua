795c068c6ddba0d519a4ca0e131cb8e3bd3386e3
Abdullah Coban <111288151+AbdullahCoban28@users.noreply.github.com>
2024-05-29 17:16:24 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -8,8 +8,8 @@ DevChat.Colors = {
     gray = "\27[90m"
 }
 
-DevChat.Router = "cSh3iRyE3_qGdRzczwkZxG7bHfFC3Kq7xhO4toPbp9k" -- Değiştirilecek: DevChat yönlendiricisinin kimliği
-DevChat.InitRoom = "1245365878657388660" -- Değiştirilecek: İlk katılınacak oda kimliği
+DevChat.Router = "cSh3iRyE3_qGdRzczwkZxG7bHfFC3Kq7xhO4toPbp9k" 
+DevChat.InitRoom = "1245365878657388660" 
 DevChat.LastSend = DevChat.InitRoom
 
 DevChat.LastReceive = {
