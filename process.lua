c6e243215fe8488fd3d1c824395927e1ce416119
Abdullah Coban <111288151+AbdullahCoban28@users.noreply.github.com>
2024-05-29 17:18:39 +0300

    Update process.lua

--- process.lua
+++ process.lua
@@ -1,1 +1,18 @@
+local json = require("json")
+
+Handlers.add(
+  "Say",
+  Handlers.utils.hasMatchingTag("Action", "Say"),
+  function (msg)
+    local fromText = msg.From
+    local room = msg.Room or "default"
+    ao.send({
+      Target = ao.id,
+      Action = "SendDiscordMsg",
+      Data = msg.Data,
+      Event = "Message in " .. room,
+      OriginatingFrom = fromText
+    })
+  end
+)
 
